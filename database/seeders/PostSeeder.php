c1ce354366c12f91f9dbaf4a34d5a5f196a5e092
Chong <monkey.d.chong@gmail.com>
2022-12-01 00:18:01 +0700

    install laravel 9

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Post::factory(rand(10, 100))->create();
+    }
+}
